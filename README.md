ce62ff348a3f20468f9fde4509315fdf5ad24f92
Benjamin Cheng <ben@bcheng.me>
2022-02-03 00:08:33 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Auto Disconnect Mod
+
+Prevents you from losing all your stuff when you AFK!
+Ever lose your stuff because you were AFK and got murdered by a random mob spawn
+or a "friend"? This mod would have prevented that.
+
+When you set "AFK Protection" on, this mod will disconnect you whenever you take
+damage or get moved.
+
+## License
+This mod is available under the GPLv3 license.
---
+++ src/main/java/me/bcheng/autodc/AutoDcMod.java
@@ -0,0 +1,18 @@
+package me.bcheng.autodc;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AutoDcMod implements ClientModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger("autodc");
+
+	@Override
+	public void onInitializeClient() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
