fab4bd2e27d596d85836e053453a97f1a9ff7938
Benjamin Cheng <ben@bcheng.me>
2022-02-05 15:25:16 -0500

    render hud text when afk protection is on

--- src/main/java/me/bcheng/autodc/AutoDcMod.java
+++ src/main/java/me/bcheng/autodc/AutoDcMod.java
@@ -1,6 +1,7 @@
 package me.bcheng.autodc;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.fabricmc.fabric.api.client.screen.v1.Screens;
 import net.minecraft.client.MinecraftClient;
@@ -8,13 +9,16 @@ import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.client.gui.widget.CyclingButtonWidget;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AutoDcMod implements ClientModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("autodc");
 
+    private final Text hudText = new LiteralText("AFK Protection On");
     private ClickableWidget afkButton;
     private boolean afk;
 
@@ -24,6 +28,18 @@ public class AutoDcMod implements ClientModInitializer {
 
         AutoDcEventCallback.EVENT.register(this::onDcEvent);
         ScreenEvents.AFTER_INIT.register(this::afterScreenInit);
+        HudRenderCallback.EVENT.register(this::renderHudOverlay);
+    }
+
+    private void renderHudOverlay(MatrixStack matrixStack, float tickDelta) {
+        var client = MinecraftClient.getInstance();
+
+        // Don't render if debug menu (F3) is open, or if AFK protection is off
+        if (this.afk || client.options.debugEnabled)
+            return;
+
+        var textRenderer = client.textRenderer;
+        textRenderer.draw(matrixStack, this.hudText, 0, 0, 0xFFFFFFFF);
     }
 
     private void onDcEvent(AutoDcEventSource reason) {
